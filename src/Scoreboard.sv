b94c6bff3a158f2e31fa4ea3c6eb5be3aa7dab1b
cbresteau <bresteau.c@gmail.com>
2018-01-29 15:32:02 -0500

    Correction ScoreBoard

--- src/Scoreboard.sv
+++ src/Scoreboard.sv
@@ -50,103 +50,103 @@ function Scoreboard::goldenModel(); // a verifier
 
 	// Récupère les opérations telles quelles de l'UAL ou non ?? exemple pour ADD ??
 	case(op)
-		op_add: this.test.op = this.mail_driver.operand_a + this.mail_driver.operand_b; // TODO Faire proporement 
+		op_add: this.test.op = this.mail_driver.operand_a + this.mail_driver.operand_b; // TODO Faire proporement
 		op_add_c: if (this.mail_driver.flag_carry){
-								test_op = this.mail_driver.operand_a + this.mail_driver.operand_b;
+								this.test.op = this.mail_driver.operand_a + this.mail_driver.operand_b;
 						 }
 							else{
-								 test_op = 0;
+								 this.test.op = 0;
 							 };
-		op_sub: test_op = this.mail_driver.operand_a - this.mail_driver.operand_b;
+		op_sub: this.test.op = this.mail_driver.operand_a - this.mail_driver.operand_b;
 		op_sub_c: if (this.mail_driver.flag_carry){
-								test_op = this.mail_driver.operand_a - this.mail_driver.operand_b;
+								this.test.op = this.mail_driver.operand_a - this.mail_driver.operand_b;
 						 }
 							else{
-								 test_op = 0;
+								 this.test.op = 0;
 							 };
-		op_and: {test_op = this.mail_driver.operand_a & this.mail_driver.operand_b;
-						 this.mail_driver.flag_carry = 0;
-						 this.mail_driver.flag_neg = 0;
-						 this.mail_driver.flag_aux_carry = 1;
+		op_and: {this.test.op = this.mail_driver.operand_a & this.mail_driver.operand_b;
+						 this.test.flag_carry = 0;
+						 this.test.flag_neg = 0;
+						 this.test.flag_aux_carry = 1;
 					 };
-		op_xor: {test_op = this.mail_driver.operand_a ^ this.mail_driver.operand_b;
-						 this.mail_driver.flag_carry = 0;
-						 this.mail_driver.flag_neg = 0;
-						 this.mail_driver.flag_aux_carry = 0;
+		op_xor: {this.test.op = this.mail_driver.operand_a ^ this.mail_driver.operand_b;
+						 this.test.flag_carry = 0;
+						 this.test.flag_neg = 0;
+						 this.test.flag_aux_carry = 0;
 					 };
-		op_or: {test_op = this.mail_driver.operand_a | this.mail_driver.operand_b;
-						 this.mail_driver.flag_carry = 0;
-						 this.mail_driver.flag_neg = 0;
-						 this.mail_driver.flag_aux_carry = 0;
+		op_or: {this.test.op = this.mail_driver.operand_a | this.mail_driver.operand_b;
+						 this.test.flag_carry = 0;
+						 this.test.flag_neg = 0;
+						 this.test.flag_aux_carry = 0;
 					 };
-		op_cp: { test_op =  this.mail_driver.operand_a - this.mail_driver.operand_b
-			if(tes_op == 0){
-				this.mail_driver.flag_zero = 1;
+		op_cp: { this.test.op =  this.mail_driver.operand_a - this.mail_driver.operand_b
+			if(this.test.op == 0){
+				this.test.flag_zero = 1;
 			}
-			else if ( test_op < 0 ){
-				this.mail_driver.flag_neg = 1;
+			else if ( this.test.op < 0 ){
+				this.test.flag_neg = 1;
 			}
 		};
-		op_inc: test_op = this.mail_driver.operand_b + 1;
-		op_dec: test_op = this.mail_driver.operand_b - 1;
-		op_daa: test_op = {if(this.mail_driver.flag_aux_carry == 1 || this.mail_driver.operand_a[3:0] > 9){
-			this.mail_driver.flag_carry = 1;
+		op_inc: this.test.op = this.mail_driver.operand_b + 1;
+		op_dec: this.test.op = this.mail_driver.operand_b - 1;
+		op_daa: this.test.op = {if(this.mail_driver.flag_aux_carry == 1 || this.mail_driver.operand_a[3:0] > 9){
+			this.test.flag_carry = 1;
 			this.mail_driver.operand_a[3:0] = this.mail_driver.operand_a[3:0] + 6;
 			this.mail_driver.operand_a[7:4] = this.mail_driver.operand_a[7:4] + 1;
 			}
 			if(this.mail_driver.flag_carry == 1 || this.mail_driver.operand_a[7:4] > 9){
 				this.mail_driver.operand_a[7:4] = this.mail_driver.operand_a[7:4] + 6;
 			}
-			this.mail_driver.flag_aux_carry = 0;
+			this.test.flag_aux_carry = 0;
 		};
 		op_rlca: {
-			this.mail_driver.flag_carry = this.mail_driver.operand_a[7];
-			test_op = this.mail_driver.operand_a <<< 1;
+			this.test.flag_carry = this.mail_driver.operand_a[7];
+			this.test.op = this.mail_driver.operand_a <<< 1;
 		};
 		op_rrca: {
-			this.mail_driver.flag_carry = this.mail_driver.operand_a[0];
-			test_op = this.mail_driver.operand_a >>> 1;
+			this.test.flag_carry = this.mail_driver.operand_a[0];
+			this.test.op = this.mail_driver.operand_a >>> 1;
 		};
 		op_rla: {
 			if(this.mail_driver.operand_a[7] == 0){
 			this.mail_driver.operand_a <<< 1;
 			this.mail_driver.operand_a[0] = TestResultQueue.flag_carry;
-			this.mail_driver.flag_carry = 0 ;
+			this.test.flag_carry = 0 ;
 		}
 		else{
 			this.mail_driver.operand_a <<< 1;
 			this.mail_driver.operand_a[0] = TestResultQueue.flag_carry;
-			this.mail_driver.flag_carry = 1 ;
+			this.test.flag_carry = 1 ;
 		}
-		test_op = this.mail_driver.operand_a;
+		this.test.op = this.mail_driver.operand_a;
 		};
 		op_rra: {
 			if(this.mail_driver.operand_a[0] == 0){
 			this.mail_driver.operand_a >>> 1;
 			this.mail_driver.operand_a[7] = TestResultQueue.flag_carry;
-			this.mail_driver.flag_carry = 0 ;
+			this.test.flag_carry = 0 ;
 		}
 		else{
 			this.mail_driver.operand_a >>> 1;
 			this.mail_driver.operand_a[7] = TestResultQueue.flag_carry;
-			this.mail_driver.flag_carry = 1 ;
+			this.test.flag_carry = 1 ;
 		}
-		test_op = this.mail_driver.operand_a;
+		this.test.op = this.mail_driver.operand_a;
 		};
 		op_cpl: {
-			test_op = ~this.mail_driver.operand_a;
+			this.test.op = ~this.mail_driver.operand_a;
 			// Erreur énoncé dans le carry flag qui reste id.
 		};
-		op_ccf: {test_op = ~this.mail_driver.flag_carry;
-			this.mail_driver.flag_carry = ~this.mail_driver.flag_carry;
+		op_ccf: {this.test.op = ~this.mail_driver.flag_carry;
+			this.test.flagcarry = ~this.mail_driver.flag_carry;
 		};
-		op_scf:{test_op = ~this.mail_driver.operand_a;
-			this.mail_driver.flag_carry = 1;
+		op_scf:{this.test.op = ~this.mail_driver.operand_a;
+			this.test.flagcarry = 1;
 			// Sortie pas importante seul le carry_flag importe
 		};
 	endcase
 
-return test_op;
+return this.test.op;
 
 endfunction : goldenModel;
 
